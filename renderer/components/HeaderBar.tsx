9275a096ee957b136ee44bf69ae8c13451a39c2e
Nick <nthorlind@gmail.com>
2022-09-20 23:32:24 -0600

    fix headerbar positioning

--- renderer/components/HeaderBar.tsx
+++ renderer/components/HeaderBar.tsx
@@ -25,77 +25,79 @@ export default function HeaderBar({
   let typeChoices = Object.keys(iconList).map((key, index) => key);
 
   return (
-    <div className="fixed w-full pr-4 h-14 pl-60 ">
-      <div className="flex flex-row items-center justify-between h-full px-2 py-4 space-x-2">
-        <Listbox
-          options={typeChoices}
-          selected={data.style.chartType}
-          data={data}
-          setData={setData}
-          iconMap={iconList}
-        />
-        <ColorPickerWidget
-          data={data}
-          setData={setData}
-          icon={<BsPaintBucket className="w-5 h-5" />}
-        />
-        <button
-          className="theme-button-outline"
-          onClick={(e) => {
-            LoadD3File("/hierarchical_retail_sales.csv").then(
-              (response: any) => {
-                setData({
-                  ...data,
-                  data: response["array"],
-                  cols: response["col_obj"],
-                  classifications: response["classification_obj"],
-                });
-              }
-            );
-          }}
-        >
-          Load test data
-        </button>
-        <button
-          className="theme-button-outline"
-          onClick={(e) => {
-            LoadD3File("/hierarchical_retail_sales.csv").then(
-              (response: any) => {
-                setData({
-                  ...data,
-                  data: response["array"],
-                  cols: response["col_obj"],
-                  classifications: response["classification_obj"],
-                });
-                setState([
-                  [],
-                  [],
-                  [
-                    {
-                      content: "datetime",
-                      id: "item-datetime0-1658126461166",
-                      type: "string",
-                    },
-                    {
-                      content: "state",
-                      id: "item-state0-1658126461166",
-                      type: "string",
-                    },
-                  ],
-                  [
-                    {
-                      content: "sales",
-                      id: "item-sales0-1658126461166",
-                      type: "number",
-                    },
-                  ],
-                ]);
-              }
-            );
-          }}
-        >
-          Load test data and set state
-        </button>
+    <div className="fixed w-full pr-4 h-14 pl-60">
+      <div className="flex flex-row items-center justify-between h-full px-2 py-4 pl-6 space-x-2">
+        <div className="flex flex-row w-full space-x-4">
+          <Listbox
+            options={typeChoices}
+            selected={data.style.chartType}
+            data={data}
+            setData={setData}
+            iconMap={iconList}
+          />
+          <ColorPickerWidget
+            data={data}
+            setData={setData}
+            icon={<BsPaintBucket className="w-5 h-5" />}
+          />
+          <button
+            className="theme-button-outline"
+            onClick={(e) => {
+              LoadD3File("/hierarchical_retail_sales.csv").then(
+                (response: any) => {
+                  setData({
+                    ...data,
+                    data: response["array"],
+                    cols: response["col_obj"],
+                    classifications: response["classification_obj"],
+                  });
+                }
+              );
+            }}
+          >
+            Load test data
+          </button>
+          <button
+            className="theme-button-outline"
+            onClick={(e) => {
+              LoadD3File("/hierarchical_retail_sales.csv").then(
+                (response: any) => {
+                  setData({
+                    ...data,
+                    data: response["array"],
+                    cols: response["col_obj"],
+                    classifications: response["classification_obj"],
+                  });
+                  setState([
+                    [],
+                    [],
+                    [
+                      {
+                        content: "datetime",
+                        id: "item-datetime0-1658126461166",
+                        type: "string",
+                      },
+                      {
+                        content: "state",
+                        id: "item-state0-1658126461166",
+                        type: "string",
+                      },
+                    ],
+                    [
+                      {
+                        content: "sales",
+                        id: "item-sales0-1658126461166",
+                        type: "number",
+                      },
+                    ],
+                  ]);
+                }
+              );
+            }}
+          >
+            Load test data and set state
+          </button>
+        </div>
         <Switch isOn={isOn} setIsOn={(e: any) => setIsOn(e)} />
       </div>
     </div>
